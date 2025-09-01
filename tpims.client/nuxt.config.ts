f1bb650afab85302f73e574fdd77f9b6757096af
luu0124 <a0988772875@gmail.com>
2025-09-01 17:21:23 +0800

    close dev tool

--- tpims.client/nuxt.config.ts
+++ tpims.client/nuxt.config.ts
@@ -40,7 +40,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   components: [{ path: '@/components', pathPrefix: false }],
   devtools: {
-    enabled: true,
+    enabled: false,
     timeline: {
       enabled: true,
     },
